12051efe420ce57aa6bb5cfc6ffe9ec36309d007
GianluGiarde <gianlugiarde@gmail.com>
2023-04-14 17:21:37 +0200

    add: cells 1 to 100

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-// 1 to 100 with Fizz (multiples of 3) and Buzz (multiples of 5)
+const grid = document.querySelector(".grid");
+
+// 1 to 100 with Fizz (multiples of 3), Buzz (multiples of 5) & FizzBuzz (multiples of 3 & 5)
 for (let i = 1; i <= 100; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
         console.log("fizzbuzz");
@@ -8,5 +10,18 @@ for (let i = 1; i <= 100; i++) {
         console.log("buzz");
     } else {
         console.log(i);
+    }
+}
+
+// cells 1 to 100 w FizzBuzz, Fizz & Buzz
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        grid.innerHTML += `<div class="cell fizzbuzz">FizzBuzz<div>`
+    } else if (i % 3 == 0) {
+        grid.innerHTML += `<div class="cell fizz">Fizz<div>`
+    } else if (i % 5 == 0) {
+        grid.innerHTML += `<div class="cell buzz">Buzz<div>`
+    } else {
+        grid.innerHTML += `<div class="cell">${i}<div>`
     }
 }
